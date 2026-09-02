030b0050d03f1c2474da30580cc5f5943be904be
ChiisaiChinChin <pupchikis17@gmail.com>
2026-09-02 20:40:40 +0300

    nice update

--- index.js
+++ index.js
@@ -1239,7 +1239,7 @@ async function handleCheckout(event) {
         }
 
         return {
-            name: item.name + " * " + item.quantity || item.title+ " * " + item.quantity,
+            name: item.name + item.quantity + "x " || item.title + item.quantity + "x ",
             price: Number(item.price) * Number(item.quantity || 1),
             quantity: 1,
             vatType: 1,
